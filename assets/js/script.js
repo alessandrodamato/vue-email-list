78d46a7b8c9725e7120a06270f24cf579310d315
alessandrodamato <alessandro042004@gmail.com>
2024-03-19 14:42:31 +0100

    [add] emails in list

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,32 @@ createApp({
 
   data(){
     return{
-
+      emails: [],
+      maxEmails: 10,
+      apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail'
     }
   },
 
   methods:{
 
+    getEmails(){
+      axios.get(this.apiUrl)
+      .then((res) => {
+        this.emails.push(res.data.response)
+      })
+    },
+    
+    cycleEmails(){
+      for (let i = 0; i < this.maxEmails; i++) {
+        this.getEmails()
+      }
+      console.log(this.emails);
+    }
+
   },
 
   mounted(){
-
+    this.cycleEmails();
   }
-  
+
 }).mount('#app')
