3cc176b3d2e4029b1068651151e0a383ad7726ac
alessandrodamato <alessandro042004@gmail.com>
2024-03-19 14:48:19 +0100

    [add] bonus (v-show)

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,12 +16,12 @@ createApp({
       axios.get(this.apiUrl)
       .then((res) => {
         this.emails.push(res.data.response)
-      })
+      });
     },
     
     cycleEmails(){
       for (let i = 0; i < this.maxEmails; i++) {
-        this.getEmails()
+        this.getEmails();
       }
       console.log(this.emails);
     }
